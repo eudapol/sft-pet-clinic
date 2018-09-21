de29c5a7a9ed2b9b8a9224fd35388ff2cf31e629
Polisetty <poliseud@ES.AD.ADP.com>
2018-09-21 08:06:28 +0530

    Added Visit . Closes #18

--- pet-clinic-data/src/main/java/guru/springframework/sftpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sftpetclinic/model/Owner.java
@@ -1,5 +1,18 @@
 package guru.springframework.sftpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+	
+	Set<Pet> pets;
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
+	
 
 }
